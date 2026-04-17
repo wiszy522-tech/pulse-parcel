f6d4ad225f19037f3b30502e9a1de60d9fe770f1
Pulse Parcel <wiszy522@gmail.com>
2026-04-17 10:42:39 +0100

    Fix CORS and database config

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 DJANGO_APPS = [
     'django.contrib.admin',
